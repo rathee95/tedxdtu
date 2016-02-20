fc5e532edd453a320d88ee262712cd5a4711c078
rathee95 <rathee95@gmail.com>
2016-02-20 19:28:20 +0530

    taken speakers under construction

---
+++ 1.php
@@ -0,0 +1,144 @@
+<?php include 'includes/header.html'; ?>
+<body class="home page page-id-2 page-template page-template-template-homepage page-template-template-homepage-php">
+    <div class="wrapper">
+        <?php include 'includes/navigation.php'; ?>
+        
+
+
+<div data-q_id="#portfolio" data-q_title="Portfolio List" data-q_icon="fa-list" class="wpb_row section vc_row-fluid grid_section" style='background-color:#ffffff; padding-top:93px; padding-bottom:142px; text-align:left;'>
+    <div class=" section_inner clearfix">
+        <div class='section_inner_margin clearfix'>
+            <div class="vc_span12 wpb_column column_container">
+                <div class="wpb_wrapper">
+                    <div class="custom_font_holder" style="font-family: Open Sans; font-size: 30px; line-height: 36px; font-style: normal; font-weight: 700; color: #303030; text-decoration: none; letter-spacing: 0px; padding: 0; margin: 0; text-align: center;">
+                        </p>
+                        <p>SPEAKERS</p>
+                        <p>
+                    </div>
+                    <div class="separator  small center  " style="margin-top: 19px;margin-bottom: 21px;background-color: #e2e2e2;"></div>
+                    <div class="wpb_text_column wpb_content_element ">
+                        <div class="wpb_wrapper">
+                            <p style="text-align: center;">Below are this years innovators, changemakers, wild thinkers and problem solvers.</p>
+                        </div>
+                    </div>
+                    <div class="separator  transparent center  " style="margin-top: 10px;margin-bottom: 33px;"></div>
+                    <div class='projects_holder_outer v4 portfolio_with_space'>
+                        <div class='projects_holder clearfix v4 standard'>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="wp-content/uploads/sites/9/2014/10/sonal.jpg" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey-150x150.jpg 150w, http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg 274w" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey-150x150.jpg 150w, http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg 274w" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey-150x150.jpg 150w, http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg 274w" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey-150x150.jpg 150w, http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg 274w" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey-150x150.jpg 150w, http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg 274w" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey-150x150.jpg 150w, http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg 274w" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey-150x150.jpg 150w, http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg 274w" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                            <article class='mix portfolio_category_7 '>
+                                <div class='image_holder'><a class='portfolio_link_for_touch' href='speaker1.php' target='_self'><span class='image'><img width="274" height="274" src="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg" class="attachment-portfolio-square size-portfolio-square wp-post-image" alt="toomey" srcset="http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey-150x150.jpg 150w, http://tedxcambridgeuniversity.org/wp-content/uploads/2015/12/toomey.jpg 274w" sizes="(max-width: 274px) 100vw, 274px" /></span></a><span class='text_holder'><span class='text_outer'><span class='text_inner'><span class='feature_holder'><span class="feature_holder_icons"><a class='lightbox qbutton small white' title='Christine Toomey' href='wp-content/uploads/sites/9/2014/10/sonal.jpg' data-rel='prettyPhoto[pretty_photo_gallery]'>zoom</a><a class='preview qbutton small white' href='speaker1.php' target='_self'>view</a></span></span>
+                                    </span>
+                                    </span>
+                                    </span>
+                                </div>
+                                <div class='portfolio_description'>
+                                    <h5 class="portfolio_title"><a href="http://tedxcambridgeuniversity.org/speaker/christine-toomey/" target="_self">Christine Toomey</a></h5><span class="project_category">2016 Speakers</span></div>
+                            </article>
+                                                        <div class='filler'></div>
+                            <div class='filler'></div>
+                            <div class='filler'></div>
+                            <div class='filler'></div>
+                        </div>
+                    </div>
+                    <div class="separator  transparent center  " style="margin-top: 10px;margin-bottom: 33px;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+       <?php include 'includes/footer.html'; ?>
+    </div>
+    <!-- /.wrapper -->
+    <script type='text/javascript' src='wp-includes/js/comment-reply.min.js%3Fver=4.4.2'></script>
+    <script type='text/javascript' src='wp-content/plugins/contact-form-7/includes/js/jquery.form.min.js%3Fver=3.51.0-2014.06.20'></script>
+    <script type='text/javascript'>
+    /* <![CDATA[ */
+    var _wpcf7 = {
+        "loaderUrl": "http:\/\/themes.wplook.com\/conference\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif",
+        "recaptchaEmpty": "Please verify that you are not a robot.",
+        "sending": "Sending ..."
+    };
+    /* ]]> */
+    </script>
+    <script type='text/javascript' src='wp-content/plugins/contact-form-7/includes/js/scripts.js%3Fver=4.3.1'></script>
+    <script type='text/javascript' src='wp-content/themes/conference-wpl/assets/javascript/app.js%3Fver=4.4.2'></script>
+    <script type='text/javascript' src='wp-content/themes/conference-wpl/assets/javascript/vendor.js%3Fver=4.4.2'></script>
+    <script type='text/javascript' src='wp-includes/js/wp-embed.min.js%3Fver=4.4.2'></script>
+</body>
+
+</html>
+
+
